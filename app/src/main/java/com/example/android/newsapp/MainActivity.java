0e2821d5d88bb87223d9d9f0176fb9f39c85a0d0
melaniemkwon <melaniemkwon@gmail.com>
2017-06-28 11:29:24 -0700

    Homework 2: Adding a RecyclerView to News App (#1)
    * Working on RecyclerView
    
    * RecyclerView now working
    
    * Continuing work on click listener..
    
    * Implemented click listeners to RecyclerView's items

--- app/src/main/java/com/example/android/newsapp/MainActivity.java
+++ app/src/main/java/com/example/android/newsapp/MainActivity.java
@@ -1,22 +1,39 @@
 package com.example.android.newsapp;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.example.android.newsapp.model.NewsItem;
 import com.example.android.newsapp.utilities.NetworkUtils;
+import com.example.android.newsapp.utilities.NewsAdapter;
 
+import org.json.JSONException;
 import org.w3c.dom.Text;
 
+import java.io.IOException;
+import java.lang.reflect.Array;
 import java.net.URL;
+import java.util.ArrayList;
 
+// 8. Implement NewsAdapter.ItemClickListener from the MainActivity
 public class MainActivity extends AppCompatActivity {
+    static final String TAG = "mainactivity";
 
-    private TextView mNewsTextView;
+    private RecyclerView mRecyclerView;
+    private NewsAdapter mNewsAdapter;
     private EditText mSearchBoxEditText;
     private ProgressBar mLoadingIndicator;
 
@@ -25,18 +42,54 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mNewsTextView = (TextView) findViewById(R.id.news_data);
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview_news);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        mRecyclerView.setLayoutManager(layoutManager);
+        mRecyclerView.setHasFixedSize(true);
+
         mSearchBoxEditText = (EditText) findViewById(R.id.et_search_box);
         mLoadingIndicator = (ProgressBar) findViewById(R.id.pb_loading_indicator);
 
         loadNewsData();
+
+        // 13. Pass in this as the ItemClickListener to the NewsAdapter constructor
+//        mNewsAdapter = new NewsAdapter()
+//        mRecyclerView.setAdapter(mNewsAdapter);
     }
 
     private void loadNewsData() {
-        new FetchNewsTask().execute();
+        new FetchNewsTask("").execute();
     }
 
-    public class FetchNewsTask extends AsyncTask<Void, Void, String> {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemNumber = item.getItemId();
+
+        switch (itemNumber) {
+            case R.id.search:
+                String s = mSearchBoxEditText.getText().toString();
+                FetchNewsTask task = new FetchNewsTask(s);
+                task.execute();
+                return true;
+//            case R.id.
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    class FetchNewsTask extends AsyncTask<URL, Void, ArrayList<NewsItem>> implements NewsAdapter.ItemClickListener {
+        String query;
+        ArrayList<NewsItem> data;
+
+        FetchNewsTask(String s) {
+            query = s;
+        }
 
         @Override
         protected void onPreExecute() {
@@ -45,23 +98,45 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @Override
-        protected String doInBackground(Void... params) {
+        protected ArrayList<NewsItem> doInBackground(URL... params) {
+            ArrayList<NewsItem> result = null;
             URL newsRequestURL = NetworkUtils.buildUrl();
 
             try {
-                return NetworkUtils.getResponseFromHttpUrl(newsRequestURL);
-            } catch (Exception e) {
+                String json = NetworkUtils.getResponseFromHttpUrl(newsRequestURL);
+                result = NetworkUtils.parseJSON(json);
+            } catch (IOException e) {
                 e.printStackTrace();
-                return null;
+            } catch (JSONException e) {
+                e.printStackTrace();
             }
+            return result;
         }
 
         @Override
-        protected void onPostExecute(String newsData) {
+        protected void onPostExecute(final ArrayList<NewsItem> newsData) {
+            this.data = newsData;
+            super.onPostExecute(data);
             mLoadingIndicator.setVisibility(View.INVISIBLE);
 
-            if (newsData != null) {
-                mNewsTextView.setText(newsData);
+            if (data != null) {
+                // 13. Pass in this as the ItemClickListener to the NewsAdapter constructor
+                NewsAdapter adapter = new NewsAdapter(data, this);
+                mRecyclerView.setAdapter(adapter);
+            }
+        }
+
+        // 10. Override ItemClickListener's onListItemClick method
+        @Override
+        public void onListItemClick(int clickedItemIndex) {
+            // 11. Open url link in browser
+            openWebPage(data.get(clickedItemIndex).getUrl());
+        }
+
+        public void openWebPage(String url) {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                startActivity(intent);
             }
         }
     }
---
+++ app/src/main/java/com/example/android/newsapp/utilities/NewsAdapter.java
@@ -0,0 +1,94 @@
+package com.example.android.newsapp.utilities;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.android.newsapp.R;
+import com.example.android.newsapp.model.NewsItem;
+
+import java.util.ArrayList;
+
+/**
+ * Created by melaniekwon on 6/25/17.
+ */
+
+public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.NewsAdapterViewHolder> {
+
+    private static final String TAG = NewsAdapter.class.getSimpleName();
+
+    private ArrayList<NewsItem> data;
+
+    // 3. Create final private ItemClickListener
+    final private ItemClickListener listener;
+
+    public NewsAdapter(ArrayList<NewsItem> data, ItemClickListener listener) {
+        this.data = data;
+        // 4. Add ListItemClickListener as parameter to constructor
+        this.listener = listener;
+    }
+
+    // 1. Add interface
+    public interface ItemClickListener {
+        // 2. define void method that takes an int parameter
+        void onListItemClick(int clickedItemIndex);
+    }
+
+    @Override
+    public NewsAdapterViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        Context context = viewGroup.getContext();
+        int layoutIdForListItem = R.layout.news_list_item;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        boolean attachToParent = false;
+
+        View view = inflater.inflate(layoutIdForListItem, viewGroup, attachToParent);
+
+        return new NewsAdapterViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(NewsAdapterViewHolder holder, int position) {
+        holder.bind(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    // 5. Implement OnClickListener in NewsAdapterViewHolder class
+    class NewsAdapterViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        public final TextView mNewsTitle;
+        public final TextView mNewsDescription;
+        public final TextView mNewsTime;
+
+        public NewsAdapterViewHolder(View itemView) {
+            super(itemView);
+            mNewsTitle = (TextView) itemView.findViewById(R.id.news_title);
+            mNewsDescription = (TextView) itemView.findViewById(R.id.news_description);
+            mNewsTime = (TextView) itemView.findViewById(R.id.news_time);
+            // 7. Call setOnClickListener on the View passed into the constructor
+            itemView.setOnClickListener(this);
+        }
+
+        public void bind(int pos) {
+            NewsItem newsItem = data.get(pos);
+            mNewsTitle.setText(newsItem.getTitle());
+            mNewsDescription.setText(newsItem.getDescription());
+            mNewsTime.setText(newsItem.getPublishedAt());
+        }
+
+        // 6. Override onClick, passing in clicked item's pos
+        @Override
+        public void onClick(View v) {
+            listener.onListItemClick(getAdapterPosition());
+        }
+    }
+
+    public void setData(ArrayList<NewsItem> data) {
+        this.data = data;
+    }
+}
