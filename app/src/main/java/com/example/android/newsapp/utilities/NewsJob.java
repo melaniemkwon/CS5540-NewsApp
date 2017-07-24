710f58f0cd6ce109c319eed3c49d07d42e5124b3
melaniemkwon <melaniemkwon@gmail.com>
2017-07-24 09:37:40 -0700

    Add NewsJob.java and ScheduleUtils.java

---
+++ app/src/main/java/com/example/android/newsapp/utilities/NewsJob.java
@@ -0,0 +1,83 @@
+package com.example.android.newsapp.utilities;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import com.example.android.newsapp.data.DBHelper;
+import com.example.android.newsapp.data.DBUtils;
+import com.example.android.newsapp.data.NewsItem;
+import com.firebase.jobdispatcher.JobParameters;
+import com.firebase.jobdispatcher.JobService;
+
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+
+/**
+ * Created by melaniekwon on 7/24/17.
+ */
+
+public class NewsJob extends JobService {
+    AsyncTask mBackgroundTask;
+
+    @Override
+    public boolean onStartJob(final JobParameters job) {
+        mBackgroundTask = new AsyncTask() {
+            @Override
+            protected void onPreExecute() {
+                Toast.makeText(NewsJob.this, "News refreshed", Toast.LENGTH_SHORT).show();
+                super.onPreExecute();
+            }
+
+            @Override
+            protected Object doInBackground(Object[] params) {
+                refreshArticles(NewsJob.this);
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Object o) {
+                jobFinished(job, false);
+                super.onPostExecute(o);
+
+            }
+        };
+
+        mBackgroundTask.execute();
+
+        return true;
+    }
+
+    @Override
+    public boolean onStopJob(JobParameters job) {
+
+        if (mBackgroundTask != null) mBackgroundTask.cancel(false);
+
+        return true;
+    }
+
+    private void refreshArticles(Context context) {
+        ArrayList<NewsItem> result = null;
+        URL url = NetworkUtils.buildUrl();
+
+        SQLiteDatabase db = new DBHelper(context).getWritableDatabase();
+
+        try {
+            DBUtils.deleteAll(db);
+            String json = NetworkUtils.getResponseFromHttpUrl(url);
+            result = NetworkUtils.parseJSON(json);
+            DBUtils.bulkInsert(db, result);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        db.close();
+    }
+}
